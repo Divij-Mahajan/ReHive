676fc01cc4406db8d6954ea6e2ad0913b4e7f6a3
aggrhythm <aggarwalrhythm2003@gmail.com>
2024-11-09 18:19:09 +0530

    footer logo

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -117,7 +117,7 @@ const Footer = () => {
     <footer style={footerStyle}>
       <div style={containerStyle}>
         <div style={logoContainerStyle}>
-          <img src="../public/logo.png" alt="ReHive Logo" style={logoStyle} />
+          <img src="/logo.png" alt="ReHive Logo" style={logoStyle} />
           <h2 style={headingStyle}>ReHive</h2>
         </div>
 
